56fafb68336591b48cf3bd308c5109be16af6c0b
ahmed zidan <zidanzezo17@gmail.com>
2022-11-19 21:33:27 +0200

    Add project files.

---
+++ TestApp/TestApp/RegistarationPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistarationPage : ContentPage
+    {
+        public RegistarationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(usernameEntry.Text))
+            {
+                DisplayAlert("Wrong", "please add username", "Ok!");
+                return;
+            }
+            if (string.IsNullOrEmpty(passwordEntry.Text))
+            {
+                DisplayAlert("Wrong", "please add password", "Ok!");
+                return;
+            }
+            if (string.IsNullOrEmpty(ConfirmPassEntry.Text))
+            {
+                DisplayAlert("Wrong", "please add confirm pass", "Ok!");
+                return;
+            }
+            if (string.IsNullOrEmpty(emailEntry.Text))
+            {
+                DisplayAlert("Wrong", "please add email", "Ok!");
+                return;
+            }
+
+            if(passwordEntry.Text != ConfirmPassEntry.Text)
+            {
+                DisplayAlert("Wrong", "error with password" ,"Ok!");
+                return;
+            }
+
+            Navigation.PushAsync(new MainPage());
+
+        }
+    }
+}
